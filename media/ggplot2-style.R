c3a7119eea17127330a1809ee89275794ec40735
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2023-05-01 17:04:01 -0400

    add style

---
+++ media/ggplot2-style.R
@@ -0,0 +1,19 @@
+
+options(
+  ggplot2.discrete.colour = ggokabeito::palette_okabe_ito(),
+  ggplot2.discrete.fill = ggokabeito::palette_okabe_ito(),
+  ggplot2.continuous.colour = "viridis",
+  ggplot2.continuous.fill = "viridis"
+)
+library(ggplot2)
+theme_set(
+  theme_minimal(
+    base_size = 14,
+    base_family = "sans"
+  ) %+replace% 
+    theme(
+      panel.grid.minor = element_blank(),
+      legend.position = "bottom",
+      plot.background = element_rect(colour = "black", fill = NA, size = 3)
+    )
+)
