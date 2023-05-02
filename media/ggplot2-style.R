591a189f3813463e896d6df866f89a35fe0f8040
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2023-05-01 21:51:27 -0400

    update style

--- media/ggplot2-style.R
+++ media/ggplot2-style.R
@@ -6,16 +6,27 @@ options(
   ggplot2.continuous.fill = "viridis"
 )
 library(ggplot2)
+update_geom_defaults("point", aes(color = "#3a4145", size = 2.5))
+update_geom_defaults("line", aes(color = "#3a4145", linewidth = 1.5))
+
 theme_set(
   theme_minimal(
     base_size = 14,
     base_family = "sans"
   ) %+replace% 
     theme(
+      text = element_text(color = "#3a4145"),
+      axis.line = element_blank(),
+      axis.ticks = element_blank(),
+      panel.grid = element_blank(),
+      panel.grid.major = element_line(color = "#d2d2d2"),
+      strip.background = element_rect(),
       panel.grid.minor = element_blank(),
-      legend.position = "bottom",
-      plot.background = element_rect(color = "black", fill = NA, linewidth = 3),
-      plot.caption = element_text(color = "darkgrey", hjust = 1, size = 10)
+      plot.margin = unit(c(1, 1, 1, 1), "lines"),
+      panel.background = element_rect(fill = "#fffff8"),
+      plot.background = element_rect(color = "#3a4145", fill = "#fffff8", linewidth = 3),
+      plot.caption = element_text(color = "darkgrey", hjust = 1, size = 10),
+      legend.position = "bottom"
     )
 )
 
